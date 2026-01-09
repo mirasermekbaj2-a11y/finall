9bedc5830fb8e1b617f18fc1df76e8ce90425b0c
mirasermekbaj2-a11y <mirasermekbaj2@gmail.com>
2026-01-10 03:15:36 +0500

    Add entity

---
+++ src/main/java/com/example/library/entity/Book.java
@@ -0,0 +1,28 @@
+package com.example.library.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_book")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+}
